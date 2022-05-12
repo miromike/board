d793180059bbbdf3a079a1ab7c18120d497ec88e
miromike <lee.wonhyung@gmail.com>
2022-05-13 04:53:54 +0900

    Initial commit

---
+++ src/main/java/miro/board/DemoApplication.java
@@ -0,0 +1,13 @@
+package miro.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
